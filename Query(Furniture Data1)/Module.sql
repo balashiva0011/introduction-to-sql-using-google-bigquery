ce0422269f227fd2670790557da5b4fb38cfe8ee
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 20:47:23 +0530

    sql
    sql

--- Query(Furniture Data1)/Module.sql
+++ Query(Furniture Data1)/Module.sql
@@ -6,6 +6,13 @@ aggregate: a function where multiple values are processed together to form a sin
 -----------------------------------
   Slide Examples BigQuery Example
 ----------------------------------- */
+IN furiniture data purchase_price as convert datatype NUM  to FLOAT64
+
+SELECT
+cast(purchase_price as FLOAT64) as purchase_prices 
+FROM `furniture_transation.furniture_123`  
+order by cast(purchase_price as FLOAT64) desc
+
 
 
 
