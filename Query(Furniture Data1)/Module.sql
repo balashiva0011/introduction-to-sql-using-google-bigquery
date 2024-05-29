33874f41344ea30f4418985b7d450623d26d5eb9
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 21:00:45 +0530

    Update Module.sql

--- Query(Furniture Data1)/Module.sql
+++ Query(Furniture Data1)/Module.sql
@@ -1,8 +1,8 @@
 /* ======================
      DATA- Furniture
-     aggregate function in SQL
+     cast function in SQL
 ======================
-aggregate: a function where multiple values are processed together to form a single summary statistic.
+
 -----------------------------------
   Slide Examples BigQuery Example
 ----------------------------------- */
