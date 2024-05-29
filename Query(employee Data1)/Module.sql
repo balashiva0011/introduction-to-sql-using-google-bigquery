9d2fccc36807e65434e0f0e9c1341571cc8e5bcb
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 20:41:44 +0530

    Create Module.sql

---
+++ Query(employee Data1)/Module.sql
@@ -0,0 +1,12 @@
+/* ======================
+     DATA- Furniture
+     aggregate function in SQL
+======================
+aggregate: a function where multiple values are processed together to form a single summary statistic.
+-----------------------------------
+  Slide Examples BigQuery Example
+----------------------------------- */
+
+
+
+
