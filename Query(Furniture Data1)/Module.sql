10a42aedb5cb9c35d7cb1eb8285770bffe5dca43
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 20:59:04 +0530

    Update Module.sql

--- Query(Furniture Data1)/Module.sql
+++ Query(Furniture Data1)/Module.sql
@@ -13,6 +13,17 @@ cast(purchase_price as FLOAT64) as purchase_prices
 FROM `furniture_transation.furniture_123`  
 order by cast(purchase_price as FLOAT64) desc
 
+here, 
+date only in data and time 
+
+SELECT 
+cast(date as date)as date_only,
+purchase_price  
+FROM `furniture_transation.furniture_123` 
+where
+date between '2020-12-01' and '2020-12-31'
+
+
 
 
 
