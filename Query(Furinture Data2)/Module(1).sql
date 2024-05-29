b66afe1cfd58fa3660365f2fc0deaeaee6e71318
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 21:20:39 +0530

    Update Module(1).sql

--- Query(Furinture Data2)/Module(1).sql
+++ Query(Furinture Data2)/Module(1).sql
@@ -6,3 +6,14 @@
 -----------------------------------
   Slide Examples BigQuery Example
 ----------------------------------- */
+
+sort product from  high to low price
+
+SELECT 
+product_color,
+product_price,
+purchase_size
+
+ FROM `furniture_transation.furniture_123` 
+ order by  product_price desc
+
