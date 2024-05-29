b6e7000dd73e516899062da09b75cc41300046c6
msrinath <ashwinisrinath2580@gmail.com>
2024-05-29 20:03:24 +0530

    Update basic query solving .sql

--- Query(employee Data)/basic query solving .sql
+++ Query(employee Data)/basic query solving .sql
@@ -7,3 +7,10 @@
   Live BigQuery Examples
 ------------------------- */
 
+-- Show all rows and columns from the products table
+-- Include the unique GCP project ID in the query
+
+SELECT * FROM `project-143-415413.employee.employee_t`
+
+-- The shortened form without the project ID
+SELECT * FROM 'employee.employee_t`;
