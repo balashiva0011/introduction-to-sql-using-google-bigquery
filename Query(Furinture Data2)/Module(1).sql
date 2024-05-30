952f347941eb181d70e2806e2f65c65a992c29c9
msrinath <ashwinisrinath2580@gmail.com>
2024-05-30 19:07:10 +0530

    Update Module(1).sql

--- Query(Furinture Data2)/Module(1).sql
+++ Query(Furinture Data2)/Module(1).sql
@@ -7,7 +7,7 @@
   Slide Examples BigQuery Example
 ----------------------------------- */
 
-sort product from  high to low price
+-- sort product from  high to low price
 
 SELECT 
 product_color,
@@ -17,7 +17,7 @@ purchase_size
  FROM `furniture_transation.furniture_123` 
  order by  product_price desc
 
-sort product from  low to high price
+-- sort product from  low to high price
 
 SELECT 
 product_color,
@@ -27,9 +27,11 @@ purchase_size
  FROM `furniture_transation.furniture_123` 
  order by  product_price asc
 
+-- Sort category products from high to low
+-- also known as a multi-level sort
  SELECT  
 product_color,
 product_price,
 purchase_size
-FROM `project-143-415413.furniture_transation.furniture_123` 
+FROM `furniture_transation.furniture_123` 
 order by product_price,purchase_size desc
