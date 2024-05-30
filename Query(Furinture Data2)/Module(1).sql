848817cd4bfa020fc2276ba066f15ceb297f9b57
msrinath <ashwinisrinath2580@gmail.com>
2024-05-30 19:09:49 +0530

    Update Module(1).sql

--- Query(Furinture Data2)/Module(1).sql
+++ Query(Furinture Data2)/Module(1).sql
@@ -27,7 +27,7 @@ purchase_size
  FROM `furniture_transation.furniture_123` 
  order by  product_price asc
 
--- Sort category products from high to low
+-- Sort category product from high to low
 -- also known as a multi-level sort
  SELECT  
 product_color,
@@ -35,3 +35,12 @@ product_price,
 purchase_size
 FROM `furniture_transation.furniture_123` 
 order by product_price,purchase_size desc
+
+-- Which products from each category have the highest discount?
+
+SELECT  
+product_color,
+product_price,
+purchase_size
+FROM `project-143-415413.furniture_transation.furniture_123` 
+order by product_color, product_price desc
