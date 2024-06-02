3e16529b68b8102cba0b5926087298796efa96a2
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 19:15:09 +0530

    Update Module(4).sql

--- Query(Warehouse4)/Module(4).sql
+++ Query(Warehouse4)/Module(4).sql
@@ -6,3 +6,18 @@
 ------------------------
   Live BigQuery Example
 ------------------------ */
+
+
+-- Identify the top 5 warehouse IDs by employee joining  and show their total employee count
+
+
+SELECT
+  warehouse_id,
+  SUM(employee_total) AS total_sales,
+  RANK() OVER (
+    ORDER BY SUM(employee_total) DESC
+  ) AS sales_rank
+FROM `project-143-415413.warehouse.warehouse_3456`
+GROUP BY warehouse_id
+QUALIFY sales_rank <= 5
+ORDER BY sales_rank;
