b838dc759a9c9137b184cc3ee649d71053405989
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 21:02:25 +0530

    Update Module(3).sql

--- Query(employee Data4)/Module(3).sql
+++ Query(employee Data4)/Module(3).sql
@@ -14,6 +14,6 @@ employee.role as employee_role,
 department.name as department_name
 FROM `employee.departments_t` as department
 Right join  
-`project-143-415413.employee.employee_t` as employee 
+`employee.employee_t` as employee 
 on 
 employee.department_id = department.department_id
