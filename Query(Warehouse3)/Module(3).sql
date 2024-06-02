d3d5143543443406b8a160020241c021f4df1f9e
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 18:51:19 +0530

    Create Module(3).sql

---
+++ Query(Warehouse3)/Module(3).sql
@@ -0,0 +1,36 @@
+/* ==========================
+    Chapter 2 - Movie 7
+    Calculation Functions(subquery)
+  ===========================
+
+---------------------------
+  Lesson BigQuery Examples
+--------------------------- */
+
+--Scenario-4
+--create new field with exisitng fields
+
+SELECT
+    starttime,
+    start_station_id,
+    tripduration,
+    (
+        SELECT ROUND(AVG(tripduration),2)
+        FROM bigquery-public-data.new_york_citibike.citibike_trips
+        WHERE start_station_id = outer_trips.start_station_id
+    ) AS avg_duration_for_station,
+    ROUND(tripduration - (
+        SELECT AVG(tripduration)
+        FROM bigquery-public-data.new_york_citibike.citibike_trips
+        WHERE start_station_id = outer_trips.start_station_id), 2) AS difference_from_avg
+FROM bigquery-public-data.new_york_citibike.citibike_trips AS outer_trips
+ORDER BY difference_from_avg DESC
+LIMIT 25;
+
+
+
+
+
+
+
+
