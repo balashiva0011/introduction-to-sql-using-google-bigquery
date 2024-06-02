f8955900f4035970ae86d818dc613e8bb482768a
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 18:54:28 +0530

    Update Module(3).sql

--- Query(Warehouse3)/Module(3).sql
+++ Query(Warehouse3)/Module(3).sql
@@ -27,6 +27,27 @@ FROM bigquery-public-data.new_york_citibike.citibike_trips AS outer_trips
 ORDER BY difference_from_avg DESC
 LIMIT 25;
 
+--senario-5
+--similar like above and include 'where' clause 
+
+SELECT
+    tripduration,
+    start_station_id
+FROM bigquery-public-data.new_york_citibike.citibike_trips
+WHERE start_station_id IN
+    (
+        SELECT
+            top_five.start_station_id
+        FROM
+        (
+            SELECT
+                start_station_id,
+                AVG(tripduration) AS avg_duration
+            FROM bigquery-public-data.new_york_citibike.citibike_trips
+            GROUP BY start_station_id
+        ) AS top_five
+        ORDER BY avg_duration DESC
+        LIMIT 5
 
 
 
