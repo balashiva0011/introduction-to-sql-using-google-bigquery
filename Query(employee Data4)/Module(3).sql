fbe28430210fbad2f4f1b7ccbccb634558d4f583
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 20:48:02 +0530

    Create Module(3).sql

---
+++ Query(employee Data4)/Module(3).sql
@@ -0,0 +1,19 @@
+/* ==========================
+    Data- Employee
+     Right join
+  ===========================
+
+------------------------
+  Live BigQuery Example
+------------------------ */
+--Right connect  two tables with simliar id of both sides.
+
+SELECT  
+employee.name as employee_name,
+employee.role as employee_role,
+department.name as department_name
+FROM `project-143-415413.employee.departments_t` as department
+Right join  
+`project-143-415413.employee.employee_t` as employee 
+on 
+employee.department_id = department.department_id
