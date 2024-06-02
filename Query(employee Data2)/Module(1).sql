ad30fa1bf1e4287f86e3b6d52eab211f46bb0dc3
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 21:02:56 +0530

    Update Module(1).sql

--- Query(employee Data2)/Module(1).sql
+++ Query(employee Data2)/Module(1).sql
@@ -12,8 +12,8 @@ SELECT
 employee.name as employee_name,
 employee.role as employee_role,
 department.name as department_name
-FROM `project-143-415413.employee.departments_t` as department
+FROM `employee.departments_t` as department
  join  
-`project-143-415413.employee.employee_t` as employee 
+`employee.employee_t` as employee 
 on 
 employee.department_id = department.department_id
