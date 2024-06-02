b0b151243f5e24bc7bc4215ed5552fe2d0680c14
msrinath <ashwinisrinath2580@gmail.com>
2024-06-02 19:40:50 +0530

    Update Module(5).sql

--- Query(Warehouse Data5)/Module(5).sql
+++ Query(Warehouse Data5)/Module(5).sql
@@ -1,5 +1,5 @@
 /* ==========================
-    Data- Warehouse
+    Data- Warehouse and Furniture
     Calculation Functions
   ===========================
 
@@ -13,3 +13,14 @@ SELECT
     MIN(maximum_capacity) AS minimum_capacity,
     MAX(maximum_capacity) AS maximum_capacity
  FROM `warehouse.warehouse_3456` 
+
+---- What is the average revenue from products ?
+ SELECT
+    AVG(revenue) AS avg_revenue
+ FROM `furniture_transation.furniture_123` 
+
+---- What is the  average revenue along with Round function from products ?
+
+ SELECT
+   Round(AVG(purchase_price),1) AS avg_purchase_price
+ FROM `project-143-415413.furniture_transation.furniture_123` 
